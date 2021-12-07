71631898dc51dedcc52a4af2476b741db3b65673
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-12-08 03:31:57 +0900

    [Fix] テスト用コード記述

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const rootElement = document.getElementById("root");
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function NotFound() {
+  return <h2>404</h2>;
+}
 
 const App = () => {
-    const title: string = 'TypeScript React !!'
     return (
-        <>
-            <h1>{title}</h1>
-            <a href="/logout">
-                logout
-            </a>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<NotFound />} />
+
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
+export default App;
 ReactDOM.render(
     <App />,
     document.getElementById('app')
