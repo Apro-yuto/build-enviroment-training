953111f42a878a8403f320bebf14af416f715934
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-12-09 02:53:25 +0900

    [Fix] reset.css読み込み設定

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>App</title>
+        <link rel="stylesheet" src="{{ asset('css/reset.css')  }}">
     </head>
     <body class="antialiased">
         <form
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <title>yuto</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset("/css/reset.css")  }}">
     </head>
     <body class="antialiased">
         <div id="app"></div>
