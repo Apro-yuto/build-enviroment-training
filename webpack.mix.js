4744ebe5a0759823f0f423698736e753cce006cd
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-12-09 02:52:16 +0900

    [Fix] frontのエイリアス設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,22 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+            '@': __dirname + '/resources'
+        },
+    },
+})
+
+mix.options({
+    enableCssModules: true,
+});
 
 mix.ts('resources/js/app.tsx', 'public/js')
     .react()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
 
 if (mix.inProduction()){
     mix.version();
